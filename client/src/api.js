96be9a370c088fc228a59459e8410977d77f0f61
Antoine Marc <antoinefl_marc@yahoo.fr>
2018-03-20 20:08:33 +0100

    Clean and ready-to-start React Boilerplate

--- client/src/api.js
+++ client/src/api.js
@@ -12,67 +12,67 @@ const errHandler = err => {
 export default {
   service: service,
   
-  getCountries() {
-    return service
-    .get('/countries')
-    .then(res => res.data)
-    .catch(errHandler);
-  },
+  // getCountries() {
+  //   return service
+  //   .get('/countries')
+  //   .then(res => res.data)
+  //   .catch(errHandler);
+  // },
 
-  postCountries(data) {
-    return service
-      .post('/countries', data)
-      .then(res => res.data)
-      .catch(errHandler);
-  },
+  // postCountries(data) {
+  //   return service
+  //     .post('/countries', data)
+  //     .then(res => res.data)
+  //     .catch(errHandler);
+  // },
   
-  getSecret() {
-    return service
-      .get('/secret')
-      .then(res => res.data)
-      .catch(errHandler);
-  },
+  // getSecret() {
+  //   return service
+  //     .get('/secret')
+  //     .then(res => res.data)
+  //     .catch(errHandler);
+  // },
   
-  signup(userInfo) {
-    return service
-      .post('/signup', userInfo)
-      .then(res => res.data)
-      .catch(errHandler);
-  },
+  // signup(userInfo) {
+  //   return service
+  //     .post('/signup', userInfo)
+  //     .then(res => res.data)
+  //     .catch(errHandler);
+  // },
 
-  login(email, password) {
-    return service
-      .post('/login', {
-        email,
-        password,
-      })
-      .then(res => {
-        console.log("DEBUG res", res)
-        const { data } = res;
-        localStorage.setItem('user', JSON.stringify(data));
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.token;
-        return data;
-      })
-      .catch(errHandler);
-  },
+  // login(email, password) {
+  //   return service
+  //     .post('/login', {
+  //       email,
+  //       password,
+  //     })
+  //     .then(res => {
+  //       console.log("DEBUG res", res)
+  //       const { data } = res;
+  //       localStorage.setItem('user', JSON.stringify(data));
+  //       axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.token;
+  //       return data;
+  //     })
+  //     .catch(errHandler);
+  // },
 
-  logout() {
-    delete axios.defaults.headers.common['Authorization'];
-    localStorage.removeItem('user');
-  },
+  // logout() {
+  //   delete axios.defaults.headers.common['Authorization'];
+  //   localStorage.removeItem('user');
+  // },
 
-  loadUser() {
-    const userData = localStorage.getItem('user');
-    if (!userData) return false;
-    const user = JSON.parse(userData);
-    if (user.token && user.name) {
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + user.token;
-      return user;
-    }
-    return false;
-  },
+  // loadUser() {
+  //   const userData = localStorage.getItem('user');
+  //   if (!userData) return false;
+  //   const user = JSON.parse(userData);
+  //   if (user.token && user.name) {
+  //     axios.defaults.headers.common['Authorization'] = 'Bearer ' + user.token;
+  //     return user;
+  //   }
+  //   return false;
+  // },
 
-  isLoggedIn() {
-    return localStorage.getItem('user') != null
-  }
+  // isLoggedIn() {
+  //   return localStorage.getItem('user') != null
+  // }
 };
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
-import Countries from './Countries';
-import AddCountry from './AddCountry';
-import Secret from './Secret';
-import Login from './Login';
-import Signup from './Signup';
 import api from '../api';
 import logo from '../logo.svg';
 import './App.css';
@@ -14,9 +9,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      countries: []
+      // countries: []
     }
-    api.loadUser();
   }
 
   handleLogoutClick(e) {
@@ -28,22 +22,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Countries</h1>
+          <h1 className="App-title">Welcome to Orion</h1>
           <Link to="/">Home</Link> 
-          <Link to="/countries">Countries</Link> 
-          <Link to="/add-country">Add country</Link> 
-          {!api.isLoggedIn() && <Link to="/signup">Signup</Link> }
-          {!api.isLoggedIn() && <Link to="/login">Login</Link> }
-          {api.isLoggedIn() && <Link to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</Link> }
-          <Link to="/secret">Secret</Link> 
         </header>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/countries" component={Countries} />
-          <Route path="/add-country" component={AddCountry} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/secret" component={Secret} />
           <Route render={() => <h2>404</h2>} />
         </Switch>        
       </div>
